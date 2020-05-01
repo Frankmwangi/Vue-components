7e3e73ccae9626440242c18589f1e9d88663385c
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-01 14:17:20 +0300

    event bus

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,12 +3,14 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+         <p>User Age: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
          <button @click="resetFn">Reset Name</button>
     </div>
 </template>
 
 <script>
+  import  { eventBus } from '../main.js';
     export default {
         props:{
             myName:{
@@ -17,7 +19,8 @@
                 
 
             } ,
-            resetFn: Function
+            resetFn: Function,
+            userAge: Number
         },
         methods: {
             switchName(){
@@ -26,7 +29,13 @@
             resetName() {
                 this.myName ='Max';
                 this.$emit('nameWasReset', this.myName);
+            },
+            created(){
+                eventBus.$on('ageWasEdited',(age)=>{
+                    this.userAge = age;
+                });
             }
+
         }
     }
 </script>
