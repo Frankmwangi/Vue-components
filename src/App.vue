3f67cbfc7c8895fa24f681afa5a4685c75ffad02
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-02 15:00:51 +0300

    Passin data with slots

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,18 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <button @click="selectedComponent = 'appQuote'">Quote</button>
-                <button @click="selectedComponent = 'appAuthor'">Author</button>
-                <button @click="selectedComponent = 'appNew'">New</button>
-
-
-                <hr>
-                <p>{{ selectedComponent}}</p>
-                <keep-alive>
-                <component :is="selectedComponent"><p>Default content</p></component>
-                </keep-alive>
-               <!-- <app-quote >     
-               <h2 slot="title"> {{ quoteTitle }}</h2>
-               <P slot="content">A wounderful quote</P>
-               </app-quote> -->
-          
-
-            </div>
-        </div>
+    
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue';
-      import Author from './components/Author.vue';
-        import New from './components/New.vue';
     export default {
-        data: function(){
+        data: function() {
             return{
-                quoteTitle: 'The Quote',
-                selectedComponent: 'app-quote'
+                quotes:[
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
             }
-        },
-        components:{
-        appQuote: Quote,
-        appAuthor:Author,
-        appNew:New
         }
     }
 </script>
