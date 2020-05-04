916e13e8b53eea31c179dc57582530bb0aff8550
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-04 22:09:18 +0300

    Understanding transitions in JS

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,22 @@
                          <div class="alert alert-info" v-if="show" key="info">This is some iugiuhiuhuihuiinfo</div>
                           <div class="alert alert-warning" v-else key="warning">This is some iugiuhiuhuihuiinfo</div>
                     </transition>
+                    <hr>
+                    <button class="btn btn-primary" @click=" load = !load">Load / Remove</button>
+                    <br><br>
+                    <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    >
+                        <div style="width:100px; height:100px; background-color: lightgreen" v-if="load"></div>
+                    </transition>
                    
             </div>
         </div>
@@ -40,7 +56,36 @@
         data() {
             return {
                 show: true,
+                load: true,
                 alertAnimation:'fade'
+            }
+        },
+        methods:{
+            beforeEnter(el){
+                console.log('beforeEnter');
+            },
+            enter(el,done){
+                console.log('enter');
+                done();
+            },
+            afterEnter(el){
+                console.log('afterEnter');
+            },
+            enterCancelled(el){
+                console.log('enterCancelled');
+            },
+            beforeLeave(el){
+                console.log('beforeLeave');
+            },
+            leave(el,done){
+                console.log('leave');
+                done();
+            },
+            afterLeave(el){
+                console.log('afterLeave');
+            },
+            leaveCancelled(el){
+                console.log('leaveCancelled');
             }
         }
     }
