0186b79fe497e56a62785bfd14bce471f74ac048
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-05 13:31:10 +0300

    Intercepting requests

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(VueResource);
 Vue.http.options.root ='https://vue-http-57af5.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next)=>{
+  console.log(request);
+  if(request.method == 'POST'){
+  request.method = 'PUT';
+}
+next();
+});
 new Vue({
   el: '#app',
   render: h => h(App)
