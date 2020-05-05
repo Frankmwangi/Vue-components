9c6c577bf3f08da4c6eeaf78fcf05554d29f9796
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-05 12:50:34 +0300

    sending data to firebase server

--- src/App.vue
+++ src/App.vue
@@ -2,202 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-                <hr>
-                <select v-model="alertAnimation" class="form-control">
-                    <option value="fade">Fade</option>
-                       <option value="slide">Slide</option>
-                </select>
-                    <hr>
-                    <button class="btn btn-primary" @click="show = !show">Show Alert</button>
-                    <br>
-                    <transition name="alertAnimation">
-                         <div class="alert alert-info" v-if="show">This is some iugiuhiuhuihuiinfo</div>
-                    </transition>
-                     <transition name="slide">
-                         <div class="alert alert-info" v-if="show">This is some info</div>
-                    </transition>
-                     <transition 
-                     
-                     enter-active-class="animated bounce"
-                     
-                     leave-active-class="animated shake"
-                     appear>
-                         <div class="alert alert-info" v-if="show">This is some iugiuhiuhuihuiinfo</div>
-                    </transition>
-                     <transition name="alertAnimation" mode="out-in">
-                         <div class="alert alert-info" v-if="show" key="info">This is some iugiuhiuhuihuiinfo</div>
-                          <div class="alert alert-warning" v-else key="warning">This is some iugiuhiuhuihuiinfo</div>
-                    </transition>
-                    <hr>
-                    <button class="btn btn-primary" @click=" load = !load">Load / Remove</button>
-                    <br><br>
-                    <transition
-                    @before-enter="beforeEnter"
-                    @enter="enter"
-                    @after-enter="afterEnter"
-                    @enter-cancelled="enterCancelled"
-                    
-                    @before-leave="beforeLeave"
-                    @leave="leave"
-                    @after-leave="afterLeave"
-                    @leave-cancelled="leaveCancelled"
-                    >
-                        <div style="width:100px; height:100px; background-color: lightgreen" v-if="load"></div>
-                    </transition>
-                    <hr>
-                    <br>
-                    <button class="btn btn-primary"
-                    @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert':selectedComponent ='app-success-alert'">Toogle Components</button>
-                   <br><br>
-                   <transition name="fade">
-                        <component :is="selectedComponent"@click="addItem">Add item</component>
-                   </transition>
-                   <hr>
-                   <button class="btn btn-primary"@click="addItem">Add item</button>
-                   <br><br>
-                   
-                       <ul class="list-group">
-                           <transition-group name="slide">
-                       <li class="list-group-item" v-for="(number,index) in numbers"@click="removeItem(index)"
-                       style="cursor: pointer"
-                       :key="number">{{ number }}</li>
-                       </transition-group>
-                   </ul>
-                   
-                  
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input class="form-control" type="text" v-model="user.username">
+                </div>
+                  <div class="form-group">
+                    <label>Mail</label>
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">submit</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import DangerAlert from './DangerAlert.vue'
-     import SuccessAlert from './SuccessAlert.vue'
     export default {
-        data() {
-            return {
-                show: true,
-                load: true,
-                alertAnimation:'fade',
-                elementWidth: 100,
-                selectedComponent: 'app-success-alert',
-                numbers: [1,2,3,4,5]
-            }
+        data(){
+            return{
+                user:{
+                username:'',
+                email:''
+                }
+            };
         },
         methods:{
-            beforeEnter(el){
-                console.log('beforeEnter');
-                this.elementWidth = 100;
-                el.style.width = this.elementWidth + 'px';
-            },
-            enter(el,done){
-                console.log('enter');
-                let round =1;
-                const interval = setInterval(()=>{
-                    el.style.width = (this.elementWidth + round * 10) + 'px';
-                    round++;
-                    if(round > 20){
-                        clearInterval(interval);
-                        done();
-                    }
-                },20);
-                done();
-            },
-            afterEnter(el){
-                console.log('afterEnter');
-            },
-            enterCancelled(el){
-                console.log('enterCancelled');
-            },
-            beforeLeave(el){
-                console.log('beforeLeave');
-                this.elementWidth = 300;
-                el.style.width = this.elementWidth + 'px';
-            },
-            leave(el,done){
-                  let round =1;
-                const interval = setInterval(()=>{
-                    el.style.width = (this.elementWidth - round * 10) + 'px';
-                    round++;
-                    if(round > 20){
-                        clearInterval(interval);
-                        done();
-                    }
-                },20);
-                console.log('leave');
-                
-            },
-            afterLeave(el){
-                console.log('afterLeave');
-            },
-            leaveCancelled(el){
-                console.log('leaveCancelled');
-            },
-            addItem(){
-                const pos = Math.floor(Math.random() * this.numbers.length);
-                this.numbers.splice(pos, 0,this.numbers.length + 1);
-            },
-            removeItem(index){
-                this.numbers.splice(index,1)
-            },
-            
-        },
-        components: {
-            appDangerAlert:DangerAlert,
-            appSuccessAlert: SuccessAlert
+            submit(){
+             this.$http.post('https://vue-http-57af5.firebaseio.com/data.json',this.user)
+             .then(response=>{
+                 console.log(response)
+             }, error=>{
+                 console.log(error)
+             }) ;
+            }
         }
     }
 </script>
 
 <style>
-    .fade-enter{
-        opacity:0;
-    }
-    .fade-enter-active{
-        transition: opacity 1s;
-    }
-    .fade-leave {
-        /* opacity : 1s*/
-    }
-    .fade-leave-active{
-        transition: opacity 1s;
-        opacity: 0;
-    }
-    .slide-enter{
-      opacity: 0;
-    }
-    .slide-enter-active{
-        animation: slide-in 1s ease-out forwards;
-        transition: .5s;
-    }
-    .slide-leave{
-
-    }
-    .slide-leave-active{
-        animation: slide-out 1s ease-out forwards;
-        transition: opacity 1s;
-        opacity:0;
-        position:absolute;
-    }
-    .slide-move{
-        transition:transform 1s;
-    }
-    @keyframes slide-in{
-        from{
-            transform: translateY(50px);
-        }
-        to{
-            transform: translateY(0);
-        }
-    }
-     @keyframes slide-out{
-        from{
-            transform:translateY(0);
-        }
-        to{
-            transform: translateY(20px);
-        }
-    }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource';
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(VueResource);
 new Vue({
   el: '#app',
   render: h => h(App)
