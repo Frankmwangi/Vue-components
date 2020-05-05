f00f6ba27cd79a62378b42904fcb25f04fd16305
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-05 13:16:35 +0300

    Getting data from firebase

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,12 @@
                     <input class="form-control" type="text" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get data</button>
+                <br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,7 +30,8 @@
                 user:{
                 username:'',
                 email:''
-                }
+                },
+                users:[]
             };
         },
         methods:{
@@ -35,6 +42,21 @@
              }, error=>{
                  console.log(error)
              }) ;
+            },
+            fetchData(){
+                this.$http.get('https://vue-http-57af5.firebaseio.com/data.json',this.user)
+                .then(response=> {
+                    return response.json();
+                 
+                })
+                .then(data => {
+                    const  resultArray = [];
+                    for(let key in data){
+                        resultArray.push(data[key]);
+                    }
+                    this.users = resultArray;
+                });
+
             }
         }
     }
