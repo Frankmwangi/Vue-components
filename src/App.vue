a6e78bd544fa33be30573a88a33ab2f9e4ce1990
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-05 20:07:33 +0300

    Setting up routes and loading page

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-                <div class="form-group">
-                    <label>Username</label>
-                    <input class="form-control" type="text" v-model="user.username">
-                </div>
-                  <div class="form-group">
-                    <label>Mail</label>
-                    <input class="form-control" type="text" v-model="user.email">
-                </div>
-                <button class="btn btn-primary" @click="submit">submit</button>
+                <h1>Routing</h1>
                 <hr>
-                <button class="btn btn-primary" @click="fetchData">Get data</button>
-                <br>
-                <ul class="list-group">
-                    <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}}</li>
-                </ul>
+                <router-view></router-view>
             </div>
         </div>
     </div>
@@ -25,40 +12,6 @@
 
 <script>
     export default {
-        data(){
-            return{
-                user:{
-                username:'',
-                email:''
-                },
-                users:[]
-            };
-        },
-        methods:{
-            submit(){
-             this.$http.post('https://vue-http-57af5.firebaseio.com/data.json',this.user)
-             .then(response=>{
-                 console.log(response)
-             }, error=>{
-                 console.log(error)
-             }) ;
-            },
-            fetchData(){
-                this.$http.get('https://vue-http-57af5.firebaseio.com/data.json',this.user)
-                .then(response=> {
-                    return response.json();
-                 
-                })
-                .then(data => {
-                    const  resultArray = [];
-                    for(let key in data){
-                        resultArray.push(data[key]);
-                    }
-                    this.users = resultArray;
-                });
-
-            }
-        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import { routes } from './routes';
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-Vue.use(VueResource);
-Vue.http.options.root ='https://vue-http-57af5.firebaseio.com/data.json';
-Vue.http.interceptors.push((request, next)=>{
-  console.log(request);
-  if(request.method == 'POST'){
-  request.method = 'PUT';
-}
-next(response =>{
-  response.json = () => {return {messages: response.body}}
-});
-});
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes: routes
+})
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
