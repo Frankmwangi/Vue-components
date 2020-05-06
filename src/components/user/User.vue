38872fea97c78283f49c4fe20ba29f4e81f1abcd
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-06 20:35:26 +0300

    Nesting children routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,15 +1,19 @@
 <template>
 <div>
     <h1>The User Page</h1>
-    <hr></hr>
+    <hr>
+    
     <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    <hr>
+    <router-view></router-view>
+  
     </div>
     
 
 </template>
 <script>
     export default {
-        methods: {
+    methods: {
             navigateToHome(){
                 this.$router.push('/');
             }
