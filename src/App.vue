39720d93a61566d618ea83b1b8db2eda1ef36b24
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-07 13:00:45 +0300

    Centralized state in vuex

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,28 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Routing</h1>
+                <h1>Vuex</h1>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
-                <router-view name="header-top"></router-view>
-                <transition name="slide"> <router-view></router-view></transition>
-                
-                <router-view name="header-bottom"></router-view>
+                <app-counter></app-counter>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
+      import AnotherResult from './components/AnotherResult.vue';
+
     export default {
+       
         components: {
-            appHeader: Header
+            appCounter: Counter,
+            appResult: Result,
+            appAnotherResult: AnotherResult,
         }
     }
 </script>
 
-<style>
-
-</style>
