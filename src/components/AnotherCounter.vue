00e9de19355c90c8e4b9bbbd8276015f37865d71
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-07 15:37:56 +0300

    mapping actions to methods

--- src/components/AnotherCounter.vue
+++ src/components/AnotherCounter.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <button class="btn btn-primary" @click="asyncIncrement({by:50, duration:100})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by:50, duration:100})">Decrement</button>
     </div>
 </template>
 
 <script>
- import {mapMutations} from 'vuex';
+ import {mapActions} from 'vuex';
    export default {
         methods: {
-            ...mapMutations([
-                'increment',
-                'decrement'
-            ])
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement'
+            ]),
+            increment(by){
+                this.$store.dispatch('increment',by)
+            }
            
         }
     }
